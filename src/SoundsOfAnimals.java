a0e99c0304957dcdf358cace01f32f022b866e62
radum <radumuresan625@gmail.com>
2022-12-14 12:01:16 +0200

    Implementation of playing animal sounds, dubbed SoundsOfAnimals

--- src/SoundsOfAnimals.java
+++ src/SoundsOfAnimals.java
@@ -2,21 +2,23 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
 public class SoundsOfAnimals {
 
     public static void soundOfAnimals(String filename){
         try {
+            // primeste numele unui mp3 de pe disk
             FileInputStream fos = new FileInputStream(filename);
             BufferedInputStream bof = new BufferedInputStream(fos);
 
-            Player player = new player(fos);
-            Player.play();
-
+            Player player = new Player(fos);
+            player.play();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (JavaLayerException e) {
             e.printStackTrace();
         }
-
     }
 }
